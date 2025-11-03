826b28c605c6c76a647c55d954cd317b2c69d971
NpMaster10 <67094049+NpMaster10@users.noreply.github.com>
2025-11-03 09:33:24 +0000

    Update localip_linux.c

--- src/detection/localip/localip_linux.c
+++ src/detection/localip/localip_linux.c
@@ -26,7 +26,7 @@
     #include <netinet6/in6_var.h>
 #endif
 
-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__APPLE__) || defined(__NetBSD__) || defined(__HAIKU__)
+#if defined(__FreeBSD__) || defined(__OpenBSD__) || (defined(__APPLE__) &&  && !defined(__IPHONE_OS_VERSION_MIN_REQUIRED)) || defined(__NetBSD__) || defined(__HAIKU__)
 #include <net/if_media.h>
 #include <net/if_dl.h>
 #elif !defined(__GNU__)
