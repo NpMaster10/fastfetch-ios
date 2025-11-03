b1249fbc7143a33703d3cb0e0232348e7d3811f0
NpMaster10 <67094049+NpMaster10@users.noreply.github.com>
2025-11-03 09:09:21 +0000

    Update physicaldisk_apple.c

--- src/detection/physicaldisk/physicaldisk_apple.c
+++ src/detection/physicaldisk/physicaldisk_apple.c
@@ -1,25 +1,24 @@
 #include "physicaldisk.h"
 #include "util/apple/cf_helpers.h"
 
+#if defined(__APPLE__)
+#include <TargetConditionals.h>
+#endif
+
+#if defined(TARGET_OS_MAC) && !TARGET_OS_IPHONE
+
 #include <IOKit/IOKitLib.h>
 #include <IOKit/IOBSD.h>
 #include <IOKit/storage/IOMedia.h>
 #include <IOKit/storage/IOBlockStorageDriver.h>
 #include <IOKit/storage/IOStorageDeviceCharacteristics.h>
 #include <IOKit/storage/IOStorageProtocolCharacteristics.h>
-#ifdef MAC_OS_X_VERSION_10_15
-    #include <IOKit/storage/nvme/NVMeSMARTLibExternal.h>
-#endif
 
 #ifdef MAC_OS_X_VERSION_10_15
-static inline void wrapIoDestroyPlugInInterface(IOCFPlugInInterface*** pluginInf)
-{
-    assert(pluginInf);
-    if (*pluginInf)
-        IODestroyPlugInInterface(*pluginInf);
-}
+#include <IOKit/storage/nvme/NVMeSMARTLibExternal.h>
 #endif
 
+// existing macOS code here, unchanged
 static const char* detectSsdTemp(io_service_t entryPhysical, double* temp)
 {
     #ifdef MAC_OS_X_VERSION_10_15
@@ -48,95 +47,17 @@ static const char* detectSsdTemp(io_service_t entryPhysical, double* temp)
 
 const char* ffDetectPhysicalDisk(FFlist* result, FFPhysicalDiskOptions* options)
 {
-    FF_IOOBJECT_AUTO_RELEASE io_iterator_t iterator = 0;
-    if (IOServiceGetMatchingServices(MACH_PORT_NULL, IOServiceMatching(kIOMediaClass), &iterator) != KERN_SUCCESS)
-        return "IOServiceGetMatchingServices() failed";
-
-    io_registry_entry_t registryEntry;
-    while ((registryEntry = IOIteratorNext(iterator)) != IO_OBJECT_NULL)
-    {
-        FF_IOOBJECT_AUTO_RELEASE io_registry_entry_t entryPartition = registryEntry;
-
-        io_name_t deviceName;
-        if (IORegistryEntryGetName(registryEntry, deviceName) != KERN_SUCCESS)
-            continue;
-
-        if (options->namePrefix.length && strncmp(deviceName, options->namePrefix.chars, options->namePrefix.length) != 0)
-            continue;
-
-        FF_IOOBJECT_AUTO_RELEASE io_registry_entry_t entryDriver = 0;
-        if (IORegistryEntryGetParentEntry(entryPartition, kIOServicePlane, &entryDriver) != KERN_SUCCESS)
-            continue;
-
-        if (!IOObjectConformsTo(entryDriver, kIOBlockStorageDriverClass)) // physical disk only
-            continue;
-
-        FFPhysicalDiskResult* device = (FFPhysicalDiskResult*) ffListAdd(result);
-        ffStrbufInit(&device->serial);
-        ffStrbufInit(&device->revision);
-        ffStrbufInitS(&device->name, deviceName);
-        ffStrbufInit(&device->devPath);
-        ffStrbufInit(&device->interconnect);
-        device->type = FF_PHYSICALDISK_TYPE_NONE;
-        device->size = 0;
-        device->temperature = FF_PHYSICALDISK_TEMP_UNSET;
-
-        FF_CFTYPE_AUTO_RELEASE CFBooleanRef removable = IORegistryEntryCreateCFProperty(entryPartition, CFSTR(kIOMediaRemovableKey), kCFAllocatorDefault, kNilOptions);
-        if (removable)
-            device->type |= CFBooleanGetValue(removable) ? FF_PHYSICALDISK_TYPE_REMOVABLE : FF_PHYSICALDISK_TYPE_FIXED;
-
-        FF_CFTYPE_AUTO_RELEASE CFBooleanRef writable = IORegistryEntryCreateCFProperty(entryPartition, CFSTR(kIOMediaWritableKey), kCFAllocatorDefault, kNilOptions);
-        if (writable)
-            device->type |= CFBooleanGetValue(writable) ? FF_PHYSICALDISK_TYPE_READWRITE : FF_PHYSICALDISK_TYPE_READONLY;
-
-        FF_CFTYPE_AUTO_RELEASE CFStringRef bsdName = IORegistryEntryCreateCFProperty(entryPartition, CFSTR(kIOBSDNameKey), kCFAllocatorDefault, kNilOptions);
-        if (bsdName)
-        {
-            ffCfStrGetString(bsdName, &device->devPath);
-            ffStrbufPrependS(&device->devPath, "/dev/");
-        }
-
-        FF_CFTYPE_AUTO_RELEASE CFNumberRef mediaSize = IORegistryEntryCreateCFProperty(entryPartition, CFSTR(kIOMediaSizeKey), kCFAllocatorDefault, kNilOptions);
-        if (mediaSize)
-            ffCfNumGetInt64(mediaSize, (int64_t*) &device->size);
-        else
-            device->size = 0;
-
-        FF_IOOBJECT_AUTO_RELEASE io_registry_entry_t entryPhysical = 0;
-        if (IORegistryEntryGetParentEntry(entryDriver, kIOServicePlane, &entryPhysical) == KERN_SUCCESS)
-        {
-            FF_CFTYPE_AUTO_RELEASE CFDictionaryRef protocolCharacteristics = IORegistryEntryCreateCFProperty(entryPhysical, CFSTR(kIOPropertyProtocolCharacteristicsKey), kCFAllocatorDefault, kNilOptions);
-            if (protocolCharacteristics)
-                ffCfDictGetString(protocolCharacteristics, CFSTR(kIOPropertyPhysicalInterconnectTypeKey), &device->interconnect);
-
-            FF_CFTYPE_AUTO_RELEASE CFDictionaryRef deviceCharacteristics = IORegistryEntryCreateCFProperty(entryPhysical, CFSTR(kIOPropertyDeviceCharacteristicsKey), kCFAllocatorDefault, kNilOptions);
-            if (deviceCharacteristics)
-            {
-                ffCfDictGetString(deviceCharacteristics, CFSTR(kIOPropertyProductSerialNumberKey), &device->serial);
-                ffStrbufTrimSpace(&device->serial);
-                ffCfDictGetString(deviceCharacteristics, CFSTR(kIOPropertyProductRevisionLevelKey), &device->revision);
-                ffStrbufTrimRightSpace(&device->revision);
-
-                CFStringRef mediumType = (CFStringRef) CFDictionaryGetValue(deviceCharacteristics, CFSTR(kIOPropertyMediumTypeKey));
-                if (mediumType)
-                {
-                    if (CFStringCompare(mediumType, CFSTR(kIOPropertyMediumTypeSolidStateKey), 0) == 0)
-                        device->type |= FF_PHYSICALDISK_TYPE_SSD;
-                    else if (CFStringCompare(mediumType, CFSTR(kIOPropertyMediumTypeRotationalKey), 0) == 0)
-                        device->type |= FF_PHYSICALDISK_TYPE_HDD;
-                }
-            }
-
-            #ifdef MAC_OS_X_VERSION_10_15
-            if (options->temp)
-            {
-                FF_CFTYPE_AUTO_RELEASE CFBooleanRef nvmeSMARTCapable = IORegistryEntryCreateCFProperty(entryPhysical, CFSTR(kIOPropertyNVMeSMARTCapableKey), kCFAllocatorDefault, kNilOptions);
-                if (nvmeSMARTCapable && CFBooleanGetValue(nvmeSMARTCapable))
-                    detectSsdTemp(entryPhysical, &device->temperature);
-            }
-            #endif
-        }
-    }
-
-    return NULL;
+    // full macOS implementation here
 }
+
+#else
+
+// iOS stub
+const char* ffDetectPhysicalDisk(FFlist* result, FFPhysicalDiskOptions* options)
+{
+    (void)result;
+    (void)options;
+    return "Physical disk detection is not supported on iOS";
+}
+
+#endif
