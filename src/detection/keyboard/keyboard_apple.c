592fea9740dfaf79d406e1c55df70f866c742953
NpMaster10 <67094049+NpMaster10@users.noreply.github.com>
2025-11-03 09:25:51 +0000

    Update keyboard_apple.c

--- src/detection/keyboard/keyboard_apple.c
+++ src/detection/keyboard/keyboard_apple.c
@@ -1,25 +1,20 @@
 #include "keyboard.h"
-#include "util/apple/cf_helpers.h"
 #include "util/mallocHelper.h"
+#include "util/apple/cf_helpers.h"
 
+#ifdef __APPLE__
+#include <TargetConditionals.h>
+#endif
+
+#if TARGET_OS_OSX
 #include <IOKit/IOKitLib.h>
 #include <IOKit/hid/IOHIDLib.h>
-
-static void enumSet(IOHIDDeviceRef value, FFlist* results)
-{
-    FFKeyboardDevice* device = (FFKeyboardDevice*) ffListAdd(results);
-    ffStrbufInit(&device->serial);
-    ffStrbufInit(&device->name);
-
-    CFStringRef product = IOHIDDeviceGetProperty(value, CFSTR(kIOHIDProductKey));
-    ffCfStrGetString(product, &device->name);
-
-    CFStringRef serialNumber = IOHIDDeviceGetProperty(value, CFSTR(kIOHIDSerialNumberKey));
-    ffCfStrGetString(serialNumber, &device->serial);
-}
+#endif
 
 const char* ffDetectKeyboard(FFlist* devices /* List of FFKeyboardDevice */)
 {
+#if TARGET_OS_OSX
+    // Original macOS implementation
     IOHIDManagerRef FF_CFTYPE_AUTO_RELEASE manager = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
     if (IOHIDManagerOpen(manager, kIOHIDOptionsTypeNone) != kIOReturnSuccess)
         return "IOHIDManagerOpen() failed";
@@ -39,4 +34,13 @@ const char* ffDetectKeyboard(FFlist* devices /* List of FFKeyboardDevice */)
     IOHIDManagerClose(manager, kIOHIDOptionsTypeNone);
 
     return NULL;
+#else
+    // iOS stub: just add one virtual keyboard
+    FFKeyboardDevice* device = (FFKeyboardDevice*) ffListAdd(devices);
+    ffStrbufInit(&device->name);
+    ffStrbufAppendS(&device->name, "Apple Virtual Keyboard");
+    ffStrbufInit(&device->serial);
+    ffStrbufAppendS(&device->serial, "000000");
+    return NULL;
+#endif
 }
