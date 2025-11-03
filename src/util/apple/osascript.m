a55c014cdee5de6f4574f4ed98c3837a732a91b7
NpMaster10 <67094049+NpMaster10@users.noreply.github.com>
2025-11-03 10:31:41 +0000

    Update osascript.m

--- src/util/apple/osascript.m
+++ src/util/apple/osascript.m
@@ -1,3 +1,10 @@
+
+#if defined(__APPLE__)
+    #include <TargetConditionals.h>
+#endif
+
+#if TARGET_OS_MAC && !TARGET_OS_IPHONE
+
 #include "osascript.h"
 
 #import <Foundation/Foundation.h>
@@ -15,3 +22,15 @@ bool ffOsascript(const char* input, FFstrbuf* result)
     ffStrbufSetS(result, descriptor.stringValue.UTF8String);
     return true;
 }
+
+#endif
+
+#if TARGET_OS_IPHONE
+#include "osascript.h"
+bool ffOsascript(const char* input, FFstrbuf* result)
+{
+    // Not supported on iOS
+    return false;
+}
+#endif
+
