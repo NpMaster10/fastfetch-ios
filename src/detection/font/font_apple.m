d7b785e9c00e658473b5ddee65e0b62f626d6434
NpMaster10 <67094049+NpMaster10@users.noreply.github.com>
2025-11-03 09:21:00 +0000

    Add macOS font detection support

--- src/detection/font/font_apple.m
+++ src/detection/font/font_apple.m
@@ -2,7 +2,13 @@
 #include "common/io/io.h"
 #include "font.h"
 
+#ifdef __APPLE__
+#include <TargetConditionals.h>
+#endif
+
+#if TARGET_OS_OSX
 #import <AppKit/NSFont.h>
+#endif
 
 static void generateString(FFFontResult* font)
 {
@@ -23,14 +29,26 @@ static void generateString(FFFontResult* font)
 
 const char* ffDetectFontImpl(FFFontResult* result)
 {
+#if TARGET_OS_OSX
     ffStrbufAppendS(&result->fonts[0], [NSFont systemFontOfSize:12].familyName.UTF8String);
     ffStrbufAppendS(&result->fonts[1], [NSFont userFontOfSize:12].familyName.UTF8String);
+
     #ifdef MAC_OS_X_VERSION_10_15
     ffStrbufAppendS(&result->fonts[2], [NSFont monospacedSystemFontOfSize:12 weight:400].familyName.UTF8String);
     #else
     ffStrbufAppendS(&result->fonts[2], "");
     #endif
+
     ffStrbufAppendS(&result->fonts[3], [NSFont userFixedPitchFontOfSize:12].familyName.UTF8String);
+
+#else
+    // iOS dummy values
+    ffStrbufAppendS(&result->fonts[0], "System Font");
+    ffStrbufAppendS(&result->fonts[1], "User Font");
+    ffStrbufAppendS(&result->fonts[2], "Monospaced Font");
+    ffStrbufAppendS(&result->fonts[3], "Fixed Pitch Font");
+#endif
+
     generateString(result);
 
     return NULL;
